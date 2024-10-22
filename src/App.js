1462f7169ecaf5b2ca1c4aaf7aec3b7a9ee9540e
Kishore <kishoreharry2001@gmail.com>
2024-10-22 19:45:43 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/landingPage";
-import AnotherPage from "./components/marketPage"; // Example: Add another component for routing
 import MarketPage from "./components/marketPage";
 
 function App() {
